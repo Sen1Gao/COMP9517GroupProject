628ff6a0ca7320ad8b3a8d3a573034016661a152
Sen1Gao <sengao1024@outlook.com>
2024-07-28 20:53:14 +1000

    Update for adding a new feature to save images and labels which are sampled and to copy them to custom location

--- utilities/data_generator.py
+++ utilities/data_generator.py
@@ -8,11 +8,14 @@ Created on Wed Jul 24 23:10:41 2024
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from pathlib import Path
+import shutil
 
 """
 This file is to generate the split dataset which has a uniform class distribution
 """
 
+dataset_dir=f"C:/Users/JARVIS/Documents/Projects"
 #Your own csv files folder path
 csv_file_folder_path="../WildScenes-main/data/splits/opt2d"
 
@@ -28,7 +31,6 @@ split_test_image_list=test_image_list[::intrval]
 
 #If you want to check distributions of split images, set this variable to True
 distribution_display=False
-dataset_dir=f"C:/Users/JARVIS/Documents/Projects"
 if distribution_display is True:
     combined_split_image_list=[split_train_image_list,split_val_image_list,split_test_image_list]
     class_distribution=[]
@@ -53,8 +55,58 @@ if distribution_display is True:
     plt.legend(loc='best')
     plt.show()
 
+new_dataset_path=f"{dataset_dir}/CustomWildScenes2d"
+new_dataset_dir=Path(new_dataset_path)
+if new_dataset_dir.exists() is True:
+    shutil.rmtree(new_dataset_dir.as_posix())
+new_dataset_dir.mkdir()
+Path(f"{new_dataset_path}/train").mkdir()
+Path(f"{new_dataset_path}/train/image").mkdir()
+Path(f"{new_dataset_path}/train/newIndexLabel").mkdir()
+Path(f"{new_dataset_path}/val").mkdir()
+Path(f"{new_dataset_path}/val/image").mkdir()
+Path(f"{new_dataset_path}/val/newIndexLabel").mkdir()
+Path(f"{new_dataset_path}/test").mkdir()
+Path(f"{new_dataset_path}/test/image").mkdir()
+Path(f"{new_dataset_path}/test/newIndexLabel").mkdir()
+
+
 #Saves paths of split images to where you refer to
-saving_path=f"C:/Users/JARVIS/Documents/Projects/WildScenes2d"
-np.save(f"{saving_path}/split_train_image_list.npy",split_train_image_list)
-np.save(f"{saving_path}/split_val_image_list.npy",split_val_image_list)
-np.save(f"{saving_path}/split_test_image_list.npy",split_test_image_list)
+np.save(f"{new_dataset_path}/split_train_image_list.npy",split_train_image_list)
+np.save(f"{new_dataset_path}/split_val_image_list.npy",split_val_image_list)
+np.save(f"{new_dataset_path}/split_test_image_list.npy",split_test_image_list)
+
+for i in split_train_image_list:
+    src_image_path=Path(f"{dataset_dir}/{i}")
+    image_name=src_image_path.name
+    dest_image_path=f"{new_dataset_path}/train/image/{image_name}"
+    shutil.copy(src_image_path.as_posix(), dest_image_path)
+    src_label_path=Path(src_image_path.as_posix().replace("image", "newIndexLabel"))
+    label_name=src_label_path.name
+    dest_label_path=f"{new_dataset_path}/train/newIndexLabel/{label_name}"
+    shutil.copy(src_label_path.as_posix(), dest_label_path)
+
+for i in split_val_image_list:
+    src_image_path=Path(f"{dataset_dir}/{i}")
+    image_name=src_image_path.name
+    dest_image_path=f"{new_dataset_path}/val/image/{image_name}"
+    shutil.copy(src_image_path.as_posix(), dest_image_path)
+    src_label_path=Path(src_image_path.as_posix().replace("image", "newIndexLabel"))
+    label_name=src_label_path.name
+    dest_label_path=f"{new_dataset_path}/val/newIndexLabel/{label_name}"
+    shutil.copy(src_label_path.as_posix(), dest_label_path)
+    
+for i in split_test_image_list:
+    src_image_path=Path(f"{dataset_dir}/{i}")
+    image_name=src_image_path.name
+    dest_image_path=f"{new_dataset_path}/test/image/{image_name}"
+    shutil.copy(src_image_path.as_posix(), dest_image_path)
+    src_label_path=Path(src_image_path.as_posix().replace("image", "newIndexLabel"))
+    label_name=src_label_path.name
+    dest_label_path=f"{new_dataset_path}/test/newIndexLabel/{label_name}"
+    shutil.copy(src_label_path.as_posix(), dest_label_path)
+    
+    
+    
+    
+    
